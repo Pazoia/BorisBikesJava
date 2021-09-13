c3e0d084e5171db5211f6e665613557b31cc6c3d
Paulo Azoia <pazoia.gomes@gmail.com>
2021-09-13 13:35:29 +0100

    Refactored tests to display names more clearly

--- src/test/java/BikeTest.java
+++ src/test/java/BikeTest.java
@@ -3,9 +3,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("Bike")
 public class BikeTest {
     @Test
-    @DisplayName("It checks if a Bike is working")
+    @DisplayName("checks if a Bike is working")
     void itChecksIfABikeIsWorking() {
         Bike bike = new Bike();
         Boolean bikeCondition = bike.isWorking();
--- src/test/java/DockingStationTest.java
+++ src/test/java/DockingStationTest.java
@@ -3,9 +3,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@DisplayName("DockingStation")
 public class DockingStationTest {
     @Test
-    @DisplayName("DockingStation returns a bike")
+    @DisplayName("returns a bike")
     void dockingStationReleasesABike() {
         DockingStation dockingStation = new DockingStation();
         Object bike = dockingStation.releaseBike();
